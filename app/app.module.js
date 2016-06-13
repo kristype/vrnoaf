36a6568bfb6a3a51a170c9b363cf1e7201a30d67
Kristian Wannebo <kristianw87@gmail.com>
2016-06-13 23:48:06 +0200

    Fixed default home route

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,19 @@
 ﻿var app = angular.module("app", ["ngRoute"]);
 
-app.config([
-"$routeProvider", function($routeProvider) {
+app.config(function($routeProvider) {
         $routeProvider
             .when("/", { templateUrl: "app/home/home.html", controller: "PageCtrl" })
             .when("/ts", { templateUrl: "app/ts/ts.html", controller: "PageCtrl" })
             .when("/about", { templateUrl: "app/about/about.html", controller: "PageCtrl" })
-            .otherwise("/404", { templateUrl: "app/404/404.html", controller: "PageCtrl" });
-    }
-]);
+            .when("/notfound", { templateUrl: "app/404/404.html", controller: "PageCtrl" })
+            .otherwise({ redirectTo: "/notfound" });
+    });
 
 app.controller("PageCtrl",
     function(/* $scope, $location, $http */) {
+    });
+
+app.run(
+    function($route)  {
+        $route.reload();
     });
