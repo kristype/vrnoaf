aaad233633fbe1dacf292a6981a3f9925867cac2
Kristian Wannebo <kristianw87@gmail.com>
2016-06-13 20:52:30 +0200

    Initial page commit

---
+++ app/app.module.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("app", ["ngRoute"]);
+
+app.config([
+"$routeProvider", function($routeProvider) {
+        $routeProvider
+            .when("/", { templateUrl: "app/home/home.html", controller: "PageCtrl" })
+            .when("/ts", { templateUrl: "app/ts/ts.html", controller: "PageCtrl" })
+            .when("/about", { templateUrl: "app/about/about.html", controller: "PageCtrl" })
+            .otherwise("/404", { templateUrl: "app/404/404.html", controller: "PageCtrl" });
+    }
+]);
+
+app.controller("PageCtrl",
+    function(/* $scope, $location, $http */) {
+    });
